77373e7bd387917a322875c914a558925c2b89aa
Mark <kruchmark@yandex.ru>
2017-12-09 00:43:58 +0300

    Финальная версия
    Финальная версия

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,23 @@
 var button = document.querySelector(".booking-btn");
 var popup = document.querySelector(".booking-form");
 var form = document.querySelector(".booking-form");
-var input = document.querySelector(".booking-input");
-var count = document.querySelector(".count-input");
+var input_in = document.querySelector(".booking-input-in");
+var input_out = document.querySelector(".booking-input-out");
+var count_adlt = document.querySelector(".count-input-adults");
+var count_chld = document.querySelector(".count-input-children");
 
 button.addEventListener("click", function (Event) {
     Event.preventDefault();
     popup.classList.toggle("modal-show");
+    popup.classList.remove("modal-error");
 });
 
-form.addEventListener("submit", function(Event) {
-    if (input.value || count.value) {
+form.addEventListener("submit", function (Event) {
+    if (!input_in.value || !input_out.value || !count_adlt.value || !count_chld.value) {
         Event.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
         console.log("Нужно ввести данные");
     }
 });
